6a5fae63457e8310e0dca9c1f2eeea7f6fd5fd4d
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2022-11-18 13:23:55 +0700

    Create zikraWarnetExample.java

---
+++ zikraWarnetExample.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_181122_A;
+
+/**
+ *
+ * @author user
+ */
+public class zikraWarnetExample {
+    public static void main(String[] args) {
+        WarnetRecord pelanggan = new WarnetRecord();
+
+        pelanggan.setKode(1);
+        pelanggan.setNamaPelanggan("Zikra");
+        pelanggan.setJenisPelanggan(1);
+        pelanggan.setTglMasuk(2);
+        pelanggan.setJamMasuk(5);
+        pelanggan.setJamKeluar(8);
+        pelanggan.setLama(2);
+
+        pelanggan.Print();
+        pelanggan.Pembayaran();
+
+
+
+
+    }
+
+    private void setKode(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setNamaPelanggan(String zikra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setJenisPelanggan(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setTglMasuk(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setJamMasuk(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setJamKeluar(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setLama(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void Print() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void Pembayaran() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
